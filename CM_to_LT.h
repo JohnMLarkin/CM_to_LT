961d925b1911d20bcb189a38704c1c909f32a653
John Larkin <jlarkin@whitworth.edu>
2019-07-10 13:32:03 -0700

    broadcastTelemetry added

--- CM_to_LT.h
+++ CM_to_LT.h
@@ -9,7 +9,7 @@
 #ifndef CM_TO_LT_H
 #define CM_TO_LT_H
 
-#include <XBeeAPIParser.h>
+#include <XBee900APIParser.h>
 #include <mbed.h>
 #include <rtos.h>
 #include <string> 
@@ -18,10 +18,12 @@ using namespace std;
 class CM_to_LT
 {
 private:
-  XBeeAPIParser _xbee;
+  XBee900APIParser _xbee;
 
 public:
-
+  CM_to_LT(PinName tx, PinName rx);
+  int broadcastTelemetry(float lat, float lng, float alt, float gndSpeed, float vertVel, int heading);
+  int broadcastHello();
 };
 
 #endif
